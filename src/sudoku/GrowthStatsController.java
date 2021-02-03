d001ed5b940b088559a22d353422926cbe1b86bb
ishmamt <55696609+ishmamt@users.noreply.github.com>
2021-02-03 22:50:58 +0600

    Added Graph

---
+++ src/sudoku/GrowthStatsController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author User
+ */
+public class GrowthStatsController implements Initializable {
+
+    @FXML
+    private Button backBtn;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleBackPressed(ActionEvent event) throws IOException {
+        Parent statsParent = FXMLLoader.load(getClass().getResource("Welcome.fxml"));
+        Scene statsScene = new Scene(statsParent);
+        
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(statsScene);
+    }
+    
+}
--- src/sudoku/WelcomeController.java
+++ src/sudoku/WelcomeController.java
@@ -7,6 +7,7 @@ package sudoku;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Scanner;
@@ -17,9 +18,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import static sudoku.Intermediate.*;
 
@@ -30,6 +36,13 @@ import static sudoku.Intermediate.*;
  */
 public class WelcomeController implements Initializable
 {       
+
+    @FXML
+    private Button puzzles;
+    @FXML
+    private Button exit;
+    @FXML
+    private Button srowthStatsBtn;
     @FXML
     private void clickpuzzles(ActionEvent event) throws Exception
     {
@@ -171,5 +184,69 @@ public class WelcomeController implements Initializable
         }
         
     }    
+
+    @FXML
+    private void drawGraph(ActionEvent event) throws IOException {
+//        Parent statsParent = FXMLLoader.load(getClass().getResource("growthStats.fxml"));
+//        Scene statsScene = new Scene(statsParent);
+//        
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+//        window.setScene(statsScene);
+//        window.show();
+
+
+          //Defining the x axis             
+            NumberAxis xAxis = new NumberAxis(1, 6, 1); 
+            xAxis.setLabel("Levels"); 
+
+            //Defining the y axis   
+            NumberAxis yAxis = new NumberAxis   (0, 150, 10); 
+            yAxis.setLabel("Best Time"); 
+
+            //Creating the line chart 
+            LineChart linechart = new LineChart(xAxis, yAxis);  
+
+            //Prepare XYChart.Series objects by setting data 
+            XYChart.Series series_easy = new XYChart.Series(); 
+            series_easy.setName("Easy"); 
+            
+            XYChart.Series series_med = new XYChart.Series(); 
+            series_med.setName("Medium"); 
+            
+            XYChart.Series series_hard = new XYChart.Series(); 
+            series_hard.setName("Hard"); 
+
+            for(int x = 0; x < 18; x++){
+                if(x < 6 && alltimes[x] != -1){
+                    series_easy.getData().add(new XYChart.Data((x % 6) + 1, alltimes[x]));
+                }
+                else if(x < 12 && alltimes[x] != -1){
+                    series_med.getData().add(new XYChart.Data((x % 6) + 1, alltimes[x])); 
+                }
+                else if(alltimes[x] != -1){
+                    series_hard.getData().add(new XYChart.Data((x % 6) + 1, alltimes[x]));
+                }
+            }
+            
+            //Setting the data to Line chart    
+            linechart.getData().add(series_easy);
+            linechart.getData().add(series_med);
+            linechart.getData().add(series_hard);
+
+            //Creating a Group object  
+            Group root = new Group(linechart); 
+
+            //Creating a scene object 
+            Scene scene = new Scene(root, 630, 700);  
+
+            //Setting title to the Stage 
+            stage.setTitle("Line Chart"); 
+
+            //Adding scene to the stage 
+            stage.setScene(scene);
+
+            //Displaying the contents of the stage 
+            stage.show(); 
+    }
     
 }
